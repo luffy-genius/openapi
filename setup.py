b8cf0f8588ae849e4f701daaf34c285f891f56c9
liuzhichao <2904662085@qq.com>
2022-07-01 17:39:06 +0800

    fix(): openapi

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ def get_version(package):
     return re.search("__version__ = ['\"]([^'\"]+)['\"]", init_py).group(1)
 
 
-version = get_version('src')
+version = get_version('openapi')
 
 setup(
     name='openapi',
