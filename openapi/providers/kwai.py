8a33a97d0481ca074338151fb1ea5dabb1eb1cd7
csrftoken <liuzhichao9527@gmail.com>
2024-08-15 00:10:21 +0800

    feat(kuaishou): 新增快手开放平台 api 对接

--- openapi/providers/kwai.py
+++ openapi/providers/kwai.py
@@ -1,30 +1,36 @@
+import json
 import time
+import hmac
+import hashlib
 import base64
 from typing import Optional
 from Crypto.Cipher import AES
-from cryptography.hazmat.backends import default_backend
-from cryptography.hazmat.primitives.hashes import MD5, Hash
 
 from openapi.enums import IntegerChoices
 from openapi.providers.base import BaseClient, BaseResult, Token
 
 
-def calculate_signature(params, api_key):
-    data = f'appId={params["appId"]}&timestamp={params["timestamp"]}&version={params["version"]}'
-    h = Hash(algorithm=MD5(), backend=default_backend())
-    h.update(f'{params["method"]}?{data}{api_key}'.encode('utf-8'))
-    return h.finalize().hex()
+def calculate_signature(params, secret):
+    sign_params = {
+        key: params[key]
+        for key in sorted(['access_token',  'appkey', 'method', 'param', 'signMethod', 'timestamp', 'version'])
+        if params.get(key)
+    }
+    sign_params['signSecret'] = secret
+    return base64.b64encode(hmac.new(
+        secret.encode(),
+        '&'.join(f'{k}={v}' for k, v in sign_params.items()).encode(),
+        hashlib.sha256
+    ).digest()).decode()
 
 
 class Code(IntegerChoices):
-    FAIL = -1, '失败'
-    SUCCESS = 0, '成功'
-    INVALID_WHITE_LIST = 2051, 'ip 未在白名单'
+    SUCCESS = 1, '成功'
 
 
 class Result(BaseResult):
-    success: bool
-    error_code: Optional[int]
+    result: Optional[int]
+    error: Optional[str]
     error_msg: Optional[str]
 
 
@@ -33,31 +39,36 @@ class Client(BaseClient):
     API_VERSION = 1
     API_BASE_URL = 'https://openapi.kwaixiaodian.com'
 
-    def __init__(self, app_id, secret, message_key):
+    def __init__(self, app_id, secret, sign_secret, message_key):
         super().__init__()
         self.app_id = app_id
         self.secret = secret
+        self.sign_secret = sign_secret
         self.message_key = message_key
         self.codes = Code
+        self._token = Optional[Token]
 
     def request(self, method, endpoint, action=None, params=None, data=None, token_request=False):
         public_params = {}
-        if data is not None:
+        if (params is not None or data is not None) and token_request is False:
             public_params.update(**{
-                'appkey': self.app_id, 'timestamp': str(int(time.time() * 1000)), 'access_token': '',
+                'appkey': self.app_id, 'timestamp': str(int(time.time() * 1000)),
                 'version': self.API_VERSION, 'method': action, 'signMethod': 'HMAC_SHA256'
             })
-            public_params['sign'] = calculate_signature(public_params, self.secret)
-            public_params['param'] = None
+            public_params['access_token'] = self._token.access_token
+            public_params['param'] = json.dumps(params or data)
+            public_params['sign'] = calculate_signature(public_params, self.sign_secret)
 
         request_url = f'{self.API_BASE_URL}{endpoint}'
-        response = self._request(method, request_url, params=params, json=data)
+        response = self._request(method, request_url, params=public_params, json=data)
+        print(response.status_code, response.json())
         return Result(**response.json())
 
     def decrypt(self, message):
-        crypt = base64.b64decode(message)
-        key = base64.b64decode(self.message_key)
-        block = AES.new(key, AES.MODE_CBC, iv=bytes(AES.block_size))
-        decrypt_data = block.decrypt(crypt)
+        block = AES.new(
+            base64.b64decode(self.message_key),
+            AES.MODE_CBC, iv=bytes(AES.block_size)
+        )
+        decrypt_data = block.decrypt(base64.b64decode(message))
         length = len(decrypt_data)
         return decrypt_data[:length - int(decrypt_data[length - 1])].decode()
